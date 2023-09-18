d60af9a0a96899f6ffb9e94266656e4a069a3501
osc8657 <osc8657@naver.com>
2023-09-18 15:25:00 +0900

    Update character_grass.py

--- character_grass.py
+++ character_grass.py
@@ -6,63 +6,64 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 390
+while():
+    x = 390
 
-while (x < 780):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+    while (x < 780):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 2
+        delay(0.01)
 
-y = 90
+    y = 90
 
-while (y < 560):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x, y)
-    y=y+2
-    delay(0.01)
+    while (y < 560):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        y=y+2
+        delay(0.01)
 
-while (x > 20):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    x = x - 2
-    delay(0.01)
+    while (x > 20):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x - 2
+        delay(0.01)
 
-while (y > 90):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    y=y-2
-    delay(0.01)
+    while (y > 90):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y=y-2
+        delay(0.01)
 
-while (x < 400):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+    while (x < 400):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 2
+        delay(0.01)
 
 
-r = 0
+    r = 0
 
-r=270
+    r=270
 
-while (r<360):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(300 * math.cos(r/360*2*math.pi) + x, 300 * math.sin(r/360*2*math.pi) + 300 + y)
-    r = r + 2
-    delay(0.01)
+    while (r<360):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(300 * math.cos(r/360*2*math.pi) + x, 300 * math.sin(r/360*2*math.pi) + 300 + y)
+        r = r + 2
+        delay(0.01)
 
-r=0
-while (r<270):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(300 * math.cos(r/360*2*math.pi) + x, 300 * math.sin(r/360*2*math.pi) + 300 + y)
-    r = r + 2
-    delay(0.01)
+    r=0
+    while (r<270):
+       clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(300 * math.cos(r/360*2*math.pi) + x, 300 * math.sin(r/360*2*math.pi) + 300 + y)
+        r = r + 2
+        delay(0.01)
 
 close_canvas()
